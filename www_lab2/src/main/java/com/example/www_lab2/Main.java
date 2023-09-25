128bdb2a127a16c54310f55270bb775246e35a33
catlynn0208 <foxfessor@gmail.com>
2023-09-25 21:39:20 +0700

    hien thi List Product

--- www_lab2/src/main/java/com/example/www_lab2/Main.java
+++ www_lab2/src/main/java/com/example/www_lab2/Main.java
@@ -1,9 +1,16 @@
 package com.example.www_lab2;
 
 import com.example.www_lab2.db.Connection;
+import com.example.www_lab2.model.Product;
+import com.example.www_lab2.services.ProductServices;
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Connection.getInstance().getSessionFactory();
-    }
+        List<Product> list = new ProductServices().findAll();
+        for (Product product : list) {
+            System.out.println(product.getName());
+        }
+     }
 }
